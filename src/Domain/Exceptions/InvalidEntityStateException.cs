9f254dbe40645929245727990207718c65e61ef8
Julio Cachay <julio.cachay.dev@gmail.com>
2024-02-29 15:53:30 -0800

    Added an EntityIdentity value object.

---
+++ src/Domain/Exceptions/InvalidEntityStateException.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Exceptions;
+
+public class InvalidEntityStateException : Exception
+{
+    public InvalidEntityStateException(
+        string errorMessage) : base(errorMessage)
+    {
+        
+    }
+}
---
+++ testing/Domain.Tests/ValueObjects/EntityIdentityTests.cs
@@ -0,0 +1,73 @@
+﻿using Domain.Exceptions;
+using Domain.ValueObjects;
+
+namespace Domain.Tests.ValueObjects;
+
+public class EntityIdentityTests
+{
+    [Fact]
+    public void Constructor_AssertsValueNonEmpty()
+    {
+        // ************ ARRANGE ************
+
+        // ************ ACT ************
+
+        var result = Record.Exception(() =>
+            new EntityIdentity(Guid.Empty));
+
+        // ************ ASSERT ************
+
+        Assert.NotNull(result);
+        Assert.IsType<InvalidEntityStateException>(result);
+
+    }
+
+    [Fact]
+    public void Constructor_SetsValue()
+    {
+        // ************ ARRANGE ************
+
+        var id = Guid.NewGuid();
+        
+        // ************ ACT ************
+
+        var sut = new EntityIdentity(id);
+
+        // ************ ASSERT ************
+        
+        Assert.Equal(id, sut.Value);
+
+    }
+
+    
+    [Fact]
+    public void CanCreateWithRandomValue()
+    {
+        // ************ ARRANGE ************
+
+        // ************ ACT ************
+
+        var sut = EntityIdentity.Random;
+
+        // ************ ASSERT ************
+        
+        Assert.NotEqual(Guid.Empty, sut.Value);
+
+    }
+
+    [Fact]
+    public void ToString_ReturnsValue()
+    {
+        // ************ ARRANGE ************
+
+        var sut = new EntityIdentity(Guid.NewGuid());
+        
+        // ************ ACT ************
+
+        var result = sut.ToString();
+        
+        // ************ ASSERT ************
+
+        Assert.Equal(sut.Value.ToString(), result);
+    }
+}
