80b91e244d06edaed4fe6d2516e234edde88a074
Julio Cachay <julio.cachay.dev@gmail.com>
2024-02-29 15:23:10 -0800

    Added an Api project with a Diagnostics controller and a Ping end point (returns pong)

---
+++ src/WebApi/Controllers/DiagnosticsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/diagnostics")]
+public class DiagnosticsController : ControllerBase
+{
+    [HttpGet]
+    [Route("ping")]
+    public async Task<ActionResult<string>> Ping()
+    {
+        return Ok("Pong");
+    }
+}
