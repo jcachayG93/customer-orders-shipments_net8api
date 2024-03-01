80085e57555eab3816e0f1a64bcf122763a59df3
Julio Cachay <julio.cachay.dev@gmail.com>
2024-02-29 16:06:30 -0800

    Created a NonEmptyString value object.

---
+++ src/Domain/ValueObjects/NonEmptyString.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.ValueObjects;
+
+public record NonEmptyString
+{
+    public string Value { get; }
+    
+    public NonEmptyString(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new InvalidEntityStateException("NonEmptyString must have a value.");
+        }
+
+        Value = value;
+    }
+
+    public bool IsEquivalentTo(NonEmptyString other)
+    {
+        return StringsAreEquivalent(Value, other.Value);
+    }
+
+    public bool IsEquivalentTo(string other)
+    {
+        return StringsAreEquivalent(Value, other);
+    }
+
+    private bool StringsAreEquivalent(string value1, string value2)
+    {
+        return string.Equals(value1.Trim(), value2.Trim(), StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
