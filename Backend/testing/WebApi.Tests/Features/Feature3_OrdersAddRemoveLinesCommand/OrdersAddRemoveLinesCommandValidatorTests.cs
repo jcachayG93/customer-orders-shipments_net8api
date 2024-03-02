e89a41945403cbc2eefd9fa72c23bfa9c41b6893
Julio Cachay <julio.cachay.dev@gmail.com>
2024-03-02 15:05:05 -0800

    Added the OrderAddRemoveLinesCommand validator

---
+++ Backend/testing/WebApi.Tests/Features/Feature3_OrdersAddRemoveLinesCommand/OrdersAddRemoveLinesCommandValidatorTests.cs
@@ -0,0 +1,145 @@
+﻿using WebApi.Features.OrdersAddRemoveLines;
+
+namespace WebApi.Tests.Features.Feature3_OrdersAddRemoveLinesCommand;
+
+public class OrdersAddRemoveLinesCommandValidatorTests
+{
+    private OrdersAddRemoveLinesCommandValidator CreateSut()
+    {
+        return new();
+    }
+
+    private OrdersAddRemoveLinesCommand CreateCommand(
+        params OrdersAddRemoveLinesCommand.OrderLineDto[] lines
+    )
+    {
+        return new()
+        {
+            OrderId = Guid.NewGuid(),
+            Lines = lines
+        };
+    }
+
+    [Theory]
+    [InlineData(-1, false)]
+    [InlineData(0, false)]
+    [InlineData(1, true)]
+    public void ValidatesLinesQuantityGreaterThanZero(
+        int quantity, bool shouldBeValid)
+    {
+        // ************ ARRANGE ************
+
+
+        OrdersAddRemoveLinesCommand.OrderLineDto line = new()
+        {
+            OrderLineId = Guid.NewGuid(),
+            Product = "Bolt type A",
+            Quantity = quantity,
+            UnitPrice = 10M
+        };
+
+        var command = CreateCommand(line);
+
+        var sut = CreateSut();
+
+        // ************ ACT ************
+
+        var result = sut.Validate(command);
+
+        // ************ ASSERT ************
+        
+        Assert.Equal(shouldBeValid, result.IsValid);
+
+        if (!shouldBeValid)
+        {
+            Assert.Equal("The SalesOrder Line Quantity must be greater than zero.",
+                result.Errors.First().ErrorMessage);
+        }
+    }
+
+    [Theory]
+    [InlineData(-1, false)]
+    [InlineData(0, true)]
+    [InlineData(1, true)]
+    public void ValidatesUnitPricePositive(
+        decimal unitPrice, bool shouldBeValid)
+    {
+        // ************ ARRANGE ************
+
+        OrdersAddRemoveLinesCommand.OrderLineDto line = new()
+        {
+            OrderLineId = Guid.NewGuid(),
+            Product = "Bolt A",
+            Quantity = 10,
+            UnitPrice = unitPrice
+        };
+
+        var command = CreateCommand(line);
+
+        var sut = CreateSut();
+        
+        // ************ ACT ************
+
+        var result = sut.Validate(command);
+        
+        // ************ ASSERT ************
+        
+        Assert.Equal(shouldBeValid, result.IsValid);
+
+        if (!shouldBeValid)
+        {
+            Assert.Equal("The SalesOrder Line Unit Price must be positive.",
+                result.Errors.First().ErrorMessage);
+        }
+
+    }
+
+    [Theory]
+    [InlineData("Bolt A", "Bolt B", true)]
+    [InlineData("Bolt A  ", "   Bolt A", false)]
+    [InlineData("Bolt A", "bolt a", false)]
+    public void ValidatesProductNameUnique(
+        string productName1, string productName2, bool shouldBeValid)
+    {
+        // ************ ARRANGE ************
+
+        var line1 = CreateLine(productName1);
+        var line2 = CreateLine(productName2);
+
+        var command = CreateCommand(line1, line2);
+
+        var sut = CreateSut();
+
+        // ************ ACT ************
+
+        var result = sut.Validate(command);
+
+        // ************ ASSERT ************
+        
+        Assert.Equal(shouldBeValid, result.IsValid);
+
+        if (!shouldBeValid)
+        {
+            Assert.Equal("Each SalesOrder line must have a unique product name.",
+                result.Errors.First().ErrorMessage);
+        }
+        
+        #region Details
+        
+        OrdersAddRemoveLinesCommand.OrderLineDto CreateLine(
+            string productName)
+        {
+            return new()
+            {
+                OrderLineId = Guid.NewGuid(),
+                Product = productName,
+                Quantity = 10,
+                UnitPrice = 10M
+            };
+        }
+        
+        #endregion
+        
+        
+    }
+}
