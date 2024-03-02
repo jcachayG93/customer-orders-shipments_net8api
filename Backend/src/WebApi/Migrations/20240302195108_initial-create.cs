923dc686304d451fb269e59daab42a0a8a5aae42
Julio Cachay <julio.cachay.dev@gmail.com>
2024-03-02 12:03:37 -0800

    Added migrations

---
+++ Backend/src/WebApi/Migrations/20240302195108_initial-create.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AssertInvariantsWasCalled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesOrderLine",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Product = table.Column<string>(type: "text", nullable: false),
+                    SalesOrderId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesOrderLine", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SalesOrderLine_Orders_SalesOrderId",
+                        column: x => x.SalesOrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesOrderLine_SalesOrderId",
+                table: "SalesOrderLine",
+                column: "SalesOrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesOrderLine");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
