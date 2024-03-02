1fb933ebde557ab782586fe81ba209caa7b91a8b
Julio Cachay <julio.cachay.dev@gmail.com>
2024-03-01 19:12:06 -0800

    Added GreaterThanZeroInteger value object

---
+++ src/Domain/ValueObjects/GreaterThanZeroInteger.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.ValueObjects;
+
+/// <summary>
+/// A positive, greater than zero integer number.
+/// </summary>
+public record GreaterThanZeroInteger
+{
+    public int Value { get; }
+
+    public GreaterThanZeroInteger(int value)
+    {
+        if (value <= 0)
+        {
+            throw new InvalidEntityStateException("GreaterThanZeroInteger value must be greater than zero.");
+        }
+
+        Value = value;
+    }
+}
---
+++ testing/Domain.Tests/ValueObjects/GreaterThanZeroIntegerTests.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Exceptions;
+using Domain.ValueObjects;
+
+namespace Domain.Tests.ValueObjects;
+
+public class GreaterThanZeroIntegerTests
+{
+    [Theory]
+    [InlineData(-1, true)]
+    [InlineData(0, true)]
+    [InlineData(1, false)]
+    [InlineData(int.MaxValue, false)]
+    public void Constructor_AssertsVGreaterThanZeroValue(
+        int value, bool shouldThrow)
+    {
+        // ************ ARRANGE ************
+
+        // ************ ACT ************
+
+        Exception? result = Record.Exception(() => new GreaterThanZeroInteger(value));
+
+        // ************ ASSERT ************
+
+        if (shouldThrow)
+        {
+            Assert.NotNull(result);
+            Assert.IsType<InvalidEntityStateException>(result);
+        }
+        else
+        {
+            Assert.Null(result);
+        }
+
+    }
+
+    [Fact]
+    public void Constructor_SetsValue()
+    {
+        // ************ ARRANGE ************
+        
+        // ************ ACT ************
+
+        GreaterThanZeroInteger sut = new GreaterThanZeroInteger(10);
+        
+        // ************ ASSERT ************
+
+        Assert.Equal(10, sut.Value);
+
+    }
+}
