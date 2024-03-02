089aeed90364039c8d7060fb72e969cb10160831
Julio Cachay <julio.cachay.dev@gmail.com>
2024-03-02 15:44:36 -0800

    Added extensions methods for collections (testing)

---
+++ Backend/testing/WebApi.Tests/TestCommon/TestCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Tests.TestCommon;
+
+public static class TestCollectionExtensions
+{
+    public static ICollection<T> ToCollectionWith<T>(this T item, params T[] additionalItems)
+    {
+        var result = new List<T>() { item };
+
+        foreach (var x in additionalItems)
+        {
+            result.Add(x);
+        }
+
+        return result;
+    }
+}
