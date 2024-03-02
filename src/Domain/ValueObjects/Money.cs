bd61e82a4d08919458d5bc0a62f7838a33d9713a
Julio Cachay <julio.cachay.dev@gmail.com>
2024-03-01 19:10:15 -0800

    Added Money value object

---
+++ src/Domain/ValueObjects/Money.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.ValueObjects;
+
+public record Money
+{
+    public Currency Currency { get; private set; }
+
+    public decimal Amount { get; private set; }
+
+    private Money(Currency currency, decimal amount)
+    {
+        if (amount < 0)
+        {
+            throw new InvalidEntityStateException("Money amount must be positive.");
+        }
+
+        Currency = currency;
+        Amount = amount;
+    }
+
+    public static Money CreateInDollars(decimal amount)
+    {
+        return new(Currency.Dollars, amount);
+    }
+}
+
+public enum Currency
+{
+    Dollars
+}
---
+++ testing/Domain.Tests/ValueObjects/Money_Tests.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Exceptions;
+using Domain.ValueObjects;
+
+namespace Domain.Tests.ValueObjects;
+
+public class Money_Tests
+{
+    [Theory]
+    [InlineData(-1, true)]
+    [InlineData(0, false)]
+    [InlineData(1, false)]
+    public void CreateDollars_AssertsAmountPositive(
+        decimal value, bool shouldThrow)
+    {
+        // ************ ARRANGE ************
+
+        // ************ ACT ************
+
+        var result = Record.Exception(() => Money.CreateInDollars(value));
+
+        // ************ ASSERT ************
+
+        if (shouldThrow)
+        {
+            Assert.NotNull(result);
+            Assert.IsType<InvalidEntityStateException>(result);
+        }
+        else
+        {
+            Assert.Null(result);
+        }
+
+    }
+    
+    [Fact]
+    public void CanCreateDollars()
+    {
+        // ************ ARRANGE ************
+
+        // ************ ACT ************
+
+        var result = Money.CreateInDollars(100M);
+        
+        // ************ ASSERT ************
+
+        Assert.Equal(100M, result.Amount);
+    }
+}
