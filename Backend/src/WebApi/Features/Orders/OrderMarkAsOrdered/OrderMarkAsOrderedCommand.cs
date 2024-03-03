1badaa6795560bc46bc670111f3380b02b259794
Julio Cachay <julio.cachay.dev@gmail.com>
2024-03-03 05:55:08 -0800

    Implemented OrderMarkAsOrderedCommand

---
+++ Backend/src/WebApi/Features/Orders/OrderMarkAsOrdered/OrderMarkAsOrderedCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using WebApi.Exceptions;
+using WebApi.Features.Orders.Common;
+
+namespace WebApi.Features.Orders.OrderMarkAsOrdered;
+
+public class OrderMarkAsOrderedCommand : IRequest
+{
+    public required Guid OrderId { get; init; }
+
+    public class Handler : IRequestHandler<OrderMarkAsOrderedCommand>
+    {
+        private readonly ISalesOrdersRepository _repository;
+
+        public Handler(ISalesOrdersRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(OrderMarkAsOrderedCommand request, CancellationToken cancellationToken)
+        {
+            var salesOrder = await _repository.GetByIdAsync(new(request.OrderId));
+
+            if (salesOrder is null)
+            {
+                throw new EntityNotFoundException($"SalesOrder with Id: {request.OrderId} was not found.");
+            }
+            
+            salesOrder.MarkAsOrdered();
+
+            await _repository.CommitChangesAsync();
+        }
+    }
+}
