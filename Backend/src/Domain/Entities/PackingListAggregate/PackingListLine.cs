02644a391c9ba5f06b7c67d8853b1f6db753828b
Julio Cachay <julio.cachay.dev@gmail.com>
2024-03-03 09:45:37 -0800

    Implemented PackingListAggregate

---
+++ Backend/src/Domain/Entities/PackingListAggregate/PackingListLine.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Entities.PackingListAggregate;
+
+/// <summary>
+/// Details for one particular product
+/// </summary>
+public class PackingListLine
+{
+    public Guid Id { get; private set; }
+
+    public string Product { get; private set; }
+
+    public int Quantity { get; private set; }
+
+    // For now, ef core requies a parameterless constructor, but note this one is private.
+    private PackingListLine()
+    {
+        
+    }
+    
+    public PackingListLine(Guid id, string product, int quantity)
+    {
+        Id = id;
+        Product = product;
+        Quantity = quantity;
+    }
+}
