26b3181161ea1ec41dbcf9ca3b2b5b229598cd5e
Julio Cachay <julio.cachay.dev@gmail.com>
2024-03-03 10:55:55 -0800

    Configured PackingList persistence

--- Backend/src/WebApi/AppDbContext.cs
+++ Backend/src/WebApi/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using Domain.Entities.OrderAggregate;
+using Domain.Entities.PackingListAggregate;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApi;
@@ -7,6 +8,8 @@ namespace WebApi;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<SalesOrder> SalesOrders { get; set; } = null!;
+
+    public DbSet<PackingList> PackingLists { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Backend/src/WebApi/Features/PackingLists/Common/PackingListTypeConfig.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities.PackingListAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace WebApi.Features.PackingLists.Common;
+
+public class PackingListTypeConfig : IEntityTypeConfiguration<PackingList>
+{
+    public void Configure(EntityTypeBuilder<PackingList> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id).ValueGeneratedNever();
+        builder.HasMany(e => e.Lines).WithOne();
+    }
+}
