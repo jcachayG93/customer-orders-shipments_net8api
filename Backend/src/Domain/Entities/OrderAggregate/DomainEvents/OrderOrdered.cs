1e922ed6277c374b791d383a3c3f36027f7bc3f0
Julio Cachay <julio.cachay.dev@gmail.com>
2024-03-03 11:23:15 -0800

    Updated SalesOrder to emit OrderOrdered domain event. Also, I made MarkAsOrdered idempotent to prevent the accidental creation of multiple packing lists per order.

---
+++ Backend/src/Domain/Entities/OrderAggregate/DomainEvents/OrderOrdered.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities.OrderAggregate.DomainEvents;
+
+public record OrderOrdered : IDomainEvent
+{
+    public required Guid OrderId { get; init; }
+
+    public required IEnumerable<OrderOrderedLine> Lines { get; init; }
+    public record OrderOrderedLine
+    {
+        public required Guid LineId { get; init; }
+
+        public required string Product { get; init; }
+
+        public required int Quantity { get; init; }
+    }
+}
+
